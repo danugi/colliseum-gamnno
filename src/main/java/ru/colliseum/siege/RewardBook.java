a7803bbff5b0717079bbabcebbbda93425e807bc
danugi <166210172+danugi@users.noreply.github.com>
2026-03-07 03:27:23 +0500

    feat: fix spider aggro and add permissions + real enchanted rewards

---
+++ src/main/java/ru/colliseum/siege/RewardBook.java
@@ -0,0 +1,10 @@
+package ru.colliseum.siege;
+
+public enum RewardBook {
+    BOOK_IV,
+    MENDING,
+    UNBREAKING_III,
+    EFFICIENCY_V,
+    SHARPNESS_V,
+    PROTECTION_IV
+}
---
+++ src/main/java/ru/colliseum/siege/RewardService.java
@@ -0,0 +1,89 @@
+package ru.colliseum.siege;
+
+import net.minecraft.component.DataComponentTypes;
+import net.minecraft.component.type.NbtComponent;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Text;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public final class RewardService {
+    private final Random random = new Random();
+
+    public void giveVictoryRewards(ServerPlayerEntity player, Difficulty d, MinecraftServer server) {
+        List<ItemStack> rewards = new ArrayList<>();
+        switch (d) {
+            case EASY -> { rewards.add(new ItemStack(Items.GOLD_INGOT, roll(1, 2))); if (chance(10)) rewards.add(RewardBookFactory.create(server, RewardBook.BOOK_IV)); }
+            case MEDIUM -> {
+                rewards.add(new ItemStack(Items.DIAMOND, roll(1, 2)));
+                rewards.add(new ItemStack(Items.GOLD_INGOT, roll(6, 10)));
+                rewards.add(new ItemStack(Items.GOLDEN_CARROT, roll(12, 16)));
+                if (chance(40)) rewards.add(new ItemStack(Items.ANCIENT_DEBRIS, 1));
+                if (chance(20)) rewards.add(new ItemStack(Items.GOLDEN_APPLE, 1));
+                if (chance(25)) rewards.add(RewardBookFactory.create(server, RewardBook.BOOK_IV));
+            }
+            case HARD -> {
+                rewards.add(new ItemStack(Items.DIAMOND, roll(2, 4)));
+                rewards.add(new ItemStack(Items.GOLD_INGOT, roll(10, 16)));
+                rewards.add(new ItemStack(Items.GOLDEN_CARROT, roll(16, 24)));
+                rewards.add(new ItemStack(Items.ANCIENT_DEBRIS, 1));
+                rewards.add(seal(1));
+                if (chance(50)) rewards.add(new ItemStack(Items.ANCIENT_DEBRIS, 1));
+                if (chance(35)) rewards.add(RewardBookFactory.create(server, RewardBook.MENDING));
+                if (chance(30)) rewards.add(RewardBookFactory.create(server, RewardBook.UNBREAKING_III));
+                if (chance(25)) rewards.add(RewardBookFactory.create(server, RewardBook.EFFICIENCY_V));
+                if (chance(25)) rewards.add(RewardBookFactory.create(server, RewardBook.SHARPNESS_V));
+                if (chance(20)) rewards.add(new ItemStack(Items.GOLDEN_APPLE, 1));
+                if (chance(25)) rewards.add(new ItemStack(Items.TOTEM_OF_UNDYING, 1));
+            }
+            case HARDCORE -> {
+                rewards.add(new ItemStack(Items.DIAMOND, roll(4, 6)));
+                rewards.add(new ItemStack(Items.GOLD_BLOCK, 1));
+                rewards.add(new ItemStack(Items.GOLDEN_CARROT, roll(24, 32)));
+                rewards.add(new ItemStack(Items.ANCIENT_DEBRIS, 4));
+                rewards.add(seal(2));
+                rewards.add(RewardBookFactory.create(server, RewardBook.MENDING));
+                if (chance(50)) rewards.add(new ItemStack(Items.TOTEM_OF_UNDYING, 1));
+                if (chance(40)) rewards.add(RewardBookFactory.create(server, RewardBook.MENDING));
+                if (chance(35)) rewards.add(RewardBookFactory.create(server, RewardBook.SHARPNESS_V));
+                if (chance(35)) rewards.add(RewardBookFactory.create(server, RewardBook.PROTECTION_IV));
+                if (chance(30)) rewards.add(RewardBookFactory.create(server, RewardBook.EFFICIENCY_V));
+                if (chance(25)) rewards.add(RewardBookFactory.create(server, RewardBook.UNBREAKING_III));
+                if (chance(25)) rewards.add(new ItemStack(Items.GOLDEN_APPLE, 1));
+            }
+        }
+        rewards.forEach(player::giveItemStack);
+    }
+
+    public static boolean hasSeal(ItemStack stack) {
+        NbtCompound nbt = stack.getOrDefault(DataComponentTypes.CUSTOM_DATA, NbtComponent.DEFAULT).copyNbt();
+        return nbt.getBoolean("colosseum_seal_applied").orElse(false);
+    }
+
+    public static ItemStack applySealTo(ItemStack target, String effectId) {
+        ItemStack out = target.copy();
+        NbtCompound nbt = out.getOrDefault(DataComponentTypes.CUSTOM_DATA, NbtComponent.DEFAULT).copyNbt();
+        nbt.putBoolean("colosseum_seal_applied", true);
+        nbt.putString("colosseum_seal_effect", effectId);
+        out.set(DataComponentTypes.CUSTOM_DATA, NbtComponent.of(nbt));
+        return out;
+    }
+
+    private ItemStack seal(int count) {
+        ItemStack seal = new ItemStack(Items.NETHER_STAR, count);
+        seal.set(DataComponentTypes.CUSTOM_NAME, Text.literal("§6Печать Колизея"));
+        NbtCompound nbt = new NbtCompound();
+        nbt.putBoolean("colosseum_seal_item", true);
+        seal.set(DataComponentTypes.CUSTOM_DATA, NbtComponent.of(nbt));
+        return seal;
+    }
+
+    private int roll(int min, int max) { return min + random.nextInt(max - min + 1); }
+    private boolean chance(int pct) { return random.nextInt(100) < pct; }
+}
